58366d8064d936ce0475151f60f7b6df989d8bbc
Pickhealer <47362439+Pickhealer@users.noreply.github.com>
2019-08-28 01:30:18 +0900

    init wayfindingpage

---
+++ src/components/wayfinding/WayFindingDetail.vue
@@ -0,0 +1,79 @@
+<template>
+  <div
+    class="px-4"
+  >
+    <b-row no-gutters>
+      <b-col
+        cols="5"
+        xl="12"
+      >
+        <div
+          class="waydetail__title mb-2"
+        >
+          {{ placeInfo.name }}
+        </div>
+        <div
+          class="waydetail__explanation mb-3"
+        >
+          {{ placeInfo.place }}
+          {{ placeInfo.floor }}
+          {{ placeInfo.placeDetail }}
+        </div>
+        <div>
+          {{ placeInfo.infomation }}
+        </div>
+      </b-col>
+      <b-col
+        cols="7"
+        xl="12"
+      >
+        <div
+          class="waydetail__map"
+        ></div>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+  import place from '@/assets/place.json'
+
+  export default {
+    name: "WayFindingDetail",
+    computed: {
+      placeInfo() {
+        return place.place[this.$route.params.index]
+      }
+    },
+    mounted() {
+      console.log(window.kakao)
+      // eslint-disable-next-line no-undef
+      kakao.maps.load(function () {
+        let element = document.getElementsByClassName('waydetail__map')[0]
+        console.log(element)
+        let option = {
+          // eslint-disable-next-line no-undef
+          center: new kakao.maps.LatLng(33.450701, 126.570667),
+          level: 3
+        }
+
+        // eslint-disable-next-line no-undef
+        new kakao.maps.Map(element, option)
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .waydetail__title {
+    font-size: 3rem;
+    font-weight: bold;
+  }
+  .waydetail__explanation {
+    font-size: 2rem;
+  }
+  .waydetail__map {
+    width: 450px;
+    height: 350px;
+  }
+</style>
---
+++ src/views/WayFindingPage.vue
@@ -0,0 +1,109 @@
+<template>
+  <b-container
+    class="wayfinding"
+    :fluid="!isWidthExtraLarge"
+  >
+    <b-row
+      no-gutters
+    >
+      <b-col
+        class="wayfinding__col pr-xl-3"
+        cols="12"
+        xl="7"
+      >
+        <b-input
+          class="mb-3"
+          v-model="searchString"
+          placeholder="Write What You Want To Find.."
+        />
+        <b-table
+          class="wayfinding__table"
+          head-variant="light"
+          selectable
+          select-mode="single"
+          selected-variant="primary"
+          @row-selected="showDetail"
+          :items="placeArray"
+          :fields="fields"
+          responsive
+          sticky-header
+        ></b-table>
+      </b-col>
+      <b-col
+        class="wayfinding__col"
+        cols="12"
+        xl="5"
+      >
+        <router-view/>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+  import place from "@/assets/place.json"
+
+  export default {
+    name: "WayFindingPage",
+    data() {
+      return {
+        searchString: '',
+        fields: [
+          {key: 'name', label:'Room Name', class:'text-center'},
+          {key: 'place', label:'Building Name', class:'text-center'},
+          {key: 'floor', label:'Floor', class:'text-center'},
+          {key: 'placeDetail', label:'Room Number', class:'text-center'}
+        ],
+        windowWidth: window.innerWidth
+      }
+    },
+    methods: {
+      showDetail(items) {
+        if(items.length === 0)
+          this.$router.push('/wayfinding')
+        else {
+          let index = place.place.findIndex(
+            (value) => {
+              return value.name === items[0].name
+            }
+          )
+          let address = '/wayfinding/' + index
+
+          if(address !== this.$route.path)
+            this.$router.push('/wayfinding/' + index)
+        }
+      }
+    },
+    computed: {
+      isWidthExtraLarge() {
+        return this.windowWidth >= 1200
+      },
+      placeArray() {
+        return place.place
+      }
+    },
+    mounted() {
+      window.addEventListener('resize', () => {
+        this.windowWidth = window.innerWidth
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .wayfinding {
+    padding-top: 100px;
+    margin-bottom: 50px;
+  }
+  .wayfinding__col {
+    height: 100%;
+  }
+  .wayfinding__table {
+    max-height: 330px;
+  }
+  @media (min-width: 1200px) {
+    .wayfinding__table {
+      max-height: 600px;
+    }
+  }
+</style>
